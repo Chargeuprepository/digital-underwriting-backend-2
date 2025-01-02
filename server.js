f17a9e833db912b7cedd07a985043d5c4c32bbbd
Gaurav Joshi <gaurav.j@echargeup.com>
2025-01-02 16:17:23 +0530

    added ssl

--- server.js
+++ server.js
@@ -1,3 +1,56 @@
+// import express from "express";
+// import { ApolloServer } from "apollo-server-express";
+// import {
+//   typeDefs,
+//   resolvers,
+// } from "./SchemaResolvers/ParentFolder-SRs/Parent-SR.js";
+// import redisClient from "./redisClient.js";
+// import dotenv from "dotenv";
+// import cors from "cors";
+
+// const app = express();
+// app.use(cors());
+
+// dotenv.config();
+
+// const startServer = async () => {
+//   try {
+//     const server = new ApolloServer({
+//       typeDefs,
+//       resolvers,
+//       context: () => ({
+//         redis: redisClient,
+//       }),
+//     });
+
+//     await server.start();
+//     server.applyMiddleware({ app });
+
+//     app.listen({ port: 4000 }, () => {
+//       console.log(
+//         `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+//       );
+//     });
+//   } catch (error) {
+//     console.error("Critical server error:", error);
+//     process.exit(1); // Exit process so it can restart
+//   }
+// };
+
+// // Start the server
+// startServer();
+
+// // Global process error handlers
+// process.on("uncaughtException", (err) => {
+//   console.error("Uncaught Exception:", err);
+//   process.exit(1); // Exit process on critical error
+// });
+
+// process.on("unhandledRejection", (reason, promise) => {
+//   console.error("Unhandled Rejection at:", promise, "reason:", reason);
+//   process.exit(1); // Exit process on critical promise rejection
+// });
+
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import {
@@ -7,11 +60,32 @@ import {
 import redisClient from "./redisClient.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import fs from "fs";
+import https from "https";
 
 const app = express();
 app.use(cors());
 
 dotenv.config();
+
+// Load SSL certificates
+const sslOptions = {
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/underwrite.echargeup.com/privkey.pem"
+  ),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/underwrite.echargeup.com/fullchain.pem"
+  ),
+};
+
+// Redirect HTTP to HTTPS
+const http = express();
+http.use((req, res) => {
+  res.redirect(`https://${req.headers.host}${req.url}`);
+});
+http.listen(80, () => {
+  console.log("Redirecting HTTP traffic to HTTPS...");
+});
 
 const startServer = async () => {
   try {
@@ -26,9 +100,10 @@ const startServer = async () => {
     await server.start();
     server.applyMiddleware({ app });
 
-    app.listen({ port: 4000 }, () => {
+    // Start HTTPS server
+    https.createServer(sslOptions, app).listen(443, () => {
       console.log(
-        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+        `🚀 Server ready at https://underwrite.echargeup.com${server.graphqlPath}`
       );
     });
   } catch (error) {
